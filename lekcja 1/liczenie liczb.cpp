d2c1a09e990b57b172f3f8bffec9503e57141d8e
lewarm <lewarmati@gmail.com>
2024-10-24 09:16:00 +0200

    bombowo

---
+++ lekcja 1/liczenie liczb.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+		int a, b;
+		cout << "Podaj liczbe: ";
+		cin >> a;
+		cout << "Podaj druga liczbe: ";
+		cin >> b;
+		
+		int suma = a + b;
+		int roznica = a - b;
+		int iloczyn = a * b;
+		
+		cout << "suma: " <<suma << endl;
+		cout << "roznica: " <<roznica << endl;
+		cout << "iloczyn: " <<iloczyn << endl;
+		
+		return 0;
+}
+ 
